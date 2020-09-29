40152a1dbbeb9433acdba41e4d353c772e8d3ed3
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-29 07:48:02 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,7 +7,7 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
-class SearchableMovieReviewsContainer extends Components {
+class SearchableMovieReviewsContainer extends Component {
   constructor(){
     this.state = {
       searchT: '',
