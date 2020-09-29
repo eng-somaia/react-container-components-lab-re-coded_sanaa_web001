a1f6e2201d0520ff94c4153026711971a5814278
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-29 08:34:43 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews'
 
-import MovieReviews from './MovieReviews';
+const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+            + `api-key=${NYT_API_KEY}`;
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const BASE_URL =
-  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
-  `api-key=${NYT_API_KEY}&query=`;
-
+// Code SearchableMovieReviewsContainer Here
 class SearchableMovieReviewsContainer extends Component {
-  state = {
-    searchTerm: '',
-    reviews: []
-  };
-
-  handleSearchInputChange = event =>
-    this.setState({ searchTerm: event.target.value });
+    this.state = {
+      searchT: '',
+      searchR: []
+    }
+  handleSearchIp = event =>
+    this.setState({ searchT: event.target.value });
 
   handleSubmit = event => {
     event.preventDefault();
 
-    fetch(BASE_URL.concat(this.state.searchTerm))
+    fetch(BASE_URL.concat(this.state.searchT))
       .then(res => res.json())
       .then(res => this.setState({ reviews: res.results }));
   };
@@ -28,22 +26,13 @@ class SearchableMovieReviewsContainer extends Component {
   render() {
     return (
       <div className="searchable-movie-reviews">
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="search-input">Search Movie Reviews</label>
-          <input
-            id="search-input"
-            type="text"
-            style={{ width: 300 }}
-            onChange={this.handleSearchInputChange}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        {typeof this.state.reviews === 'object' &&
-          this.state.reviews.length > 0 && <h2>Movie Review By Search:</h2>}
-        <MovieReviews reviews={this.state.reviews} />
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="search" onChange={this.handleSearchIp}/>
+        <buttontype="submit">Search</button>
+      </form>
+      <MovieReviews review={this.state.searchR}/>
       </div>
-    );
+    )
   }
 }
-
 export default SearchableMovieReviewsContainer;
