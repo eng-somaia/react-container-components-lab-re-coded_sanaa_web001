c27a363a82a171b95a2b9e4b4bf81f0f77bfa11c
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-29 08:29:28 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -11,7 +11,7 @@ class LatestMovieReviewsContainer extends Component {
   constructor(){
     super();
     this.state = {
-      latestM: [];
+      latestM: []
     }
   }
   componentDidMount() {
