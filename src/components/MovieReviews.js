92eda9994b24811ad896ac430875e0ea30009ea6
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-29 08:24:43 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -26,15 +26,11 @@ const Review = ({
     </div>
   );
 };
-const MovieReviews = (props) => {
-  return {
-    <div className="review-list">
-    {props.review.map(Review)}
-    </div>
-  }
-}
+
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
 
 MovieReviews.defaultProps = {
   reviews: []
 };
+
 export default MovieReviews;
